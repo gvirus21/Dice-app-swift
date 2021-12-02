fee3ea35c6c9276f08ad5c7c80c6d33dd18aa921
Gourav Kumar <69251030+gvirus21@users.noreply.github.com>
2021-12-02 01:49:50 -0800

    uploaded files

---
+++ Dice app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice app
+//
+//  Created by Pratik Bendale on 01/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
